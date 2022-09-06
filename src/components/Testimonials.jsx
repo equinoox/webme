4740ccecf8837115982dcbf249e6347e4b3a7568
Lazar Pavlovic <neopavlovic.lazar@gmail.com>
2022-09-06 11:11:24 +0200

    Fixing bugs

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -12,9 +12,6 @@ const Testimonials = () => (
     <div className="w-full flex justify-between items-cetner md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
       <h1 className={styles.heading2}>What people are <br className="sm:block hidden"/> saying about us.</h1>
       <div className="w-full md:mt-0 mt-6">
-        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-          Satisfied users praised our help and services.
-        </p>
       </div>
     </div>
 
